1d86ded670ee33d88219dc33d1ac4a343adff705
jun68ykt <jun68ykt@teratail>
2020-04-05 16:33:47 +0900

    Implement "dot_product_element" by using "for" loop

--- main.py
+++ main.py
@@ -13,7 +13,11 @@ mat_2 = [
 
 
 def dot_product_element(mat_a, mat_b, ri, ci):
-    return 0
+    s = 0
+    for i in range(len(mat_a[ri])):
+        s += mat_a[ri][i] * mat_b[i][ci]
+
+    return s
 
 
 res_mat = [[dot_product_element(mat_1, mat_2, row_index, col_index) for col_index in range(l)] for row_index in range(n)]
